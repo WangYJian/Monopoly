edd8644f9d9a6247114fd206b5b444021cf04aa2
zly <zhaoleyu@dianintel.cn>
2023-07-05 14:26:23 +0800

    修改了main中输入的问题，避免了无效输入攻击

--- development/main.c
+++ development/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 #include "Game.h"
 #include"Map.h"
 #include"Player.h"
-
-
 
 
 int main() {
@@ -13,15 +12,16 @@ int main() {
     Player* cur_player;
     char nums[5];
     int cash;
+    int result;
     int dice_num; // 色子的点数
     while(1){
         int i = 0,err = 0;
-        printf("请输入游戏的玩家: ");
+        printf("请输入玩家人数(2-4): ");
         scanf("%s", nums);
         // printf("%d",strlen(nums));
         if(strlen(nums) <= 4){
             for(i = 0; i< strlen(nums); i++){
-                if(nums[i] >'4' || nums[i] < '1'){
+                if(nums[i] >'4' || nums[i] <= '1'){
                     err = 1;
                     break;
                 }
@@ -30,20 +30,34 @@ int main() {
                 break;
             }
             else
-                printf("输入的数字有误! 请重新输入\n");
+                printf("输入数字有误! 请重新输入(2-4)\n");
         }
         else
-            printf("输入人数超了！请重新输入\n");
+            printf("输入数字有误! 请重新输入(2-4)\n");
     }
 
-    
-    while(1){
-        printf("请输入初始的金额: ");
-        scanf("%d",&cash);
-        if(cash > 1000 && cash <= 50000)
+    char* input = NULL;
+    size_t size = 0;
+
+    while (1) {
+        printf("请输入初始金额(1000-50000): ");
+        if (getline(&input, &size, stdin) == -1) {
+            // 处理输入错误或结束的情况
+            printf("无效输入！请重新输入(1000-50000)\n");
+            continue;
+        }
+
+        result = sscanf(input, "%d", &cash);
+        if (result == 1 && cash > 1000 && cash <= 50000) {
             break;
-        printf("输入初始化金额有误！请重新输入\n");
+        }
+        printf("输入初始化金额有误！请重新输入(1000-50000)\n");
     }
+
+    printf("初始金额为：%d\n", cash);
+
+    free(input); 
+
     game = GameInitialize(cash,nums);
     cur_player = GameStart(game);
     // cur_player->position = 23;
