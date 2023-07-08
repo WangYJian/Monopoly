0da75f05522903c8c98b54c499f05067cce9d539
kui253 <2533917115@qq.com>
2023-07-08 16:28:14 +0800

    最新的版本1.0

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
-
 from subprocess import *
 import os
 import io
 import re
 import time
 
-test_dir = "E:\c_workplace\monopoly\Monopoly\\test_file"
+test_dir = "./test_file"
 test_input_dir = test_dir+"/input"
 test_output_dir = test_dir+"/output"
+# group_4_test_dir = "./test_file/group_4_test"
 dump_dir = test_dir+"/dump"
-test_obj = "E:\c_workplace\monopoly\Monopoly\development.exe"
+test_obj = "./development.exe"
 log_dir = test_dir+"/log"
-
-max_log_file_size = 10000
-
+max_log_file_line = 10000
 
 test_cnt = 0
 pass_cnt = 0
@@ -28,9 +26,10 @@ def input_test(file:io.TextIOWrapper, demo: Popen):
 
 
 def write_log(demo: Popen, dump_file: io.TextIOWrapper):
-    out = demo.stdout.read(max_log_file_size)
-    out = out.decode('utf-8','ignore').replace("\r", "")
-    dump_file.write(out)
+    out = demo.stdout.readlines(max_log_file_line)
+    for line in out:
+    # out = out.decode().replace("/r", "")
+        dump_file.write(line.decode())
 
 
 def check_out(demo_out:io.TextIOWrapper, expect:io.TextIOWrapper, nameappend) -> bool:
@@ -61,7 +60,7 @@ def check_out(demo_out:io.TextIOWrapper, expect:io.TextIOWrapper, nameappend) ->
                 str4 = "get: "+"\033[1;33m"+ test_lines[i] +"\033[0m".replace("\n", "").replace("\r", "")
                 print(str1+":  "+str2+"    "+str3+"     "+str4)
                 return False
-    print("\033[1;34m"+"test"+nameappend+"\033[0m \033[1;32m pass \033[0m")
+    print("\033[1;34m"+"test  "+nameappend+"\033[0m \033[1;32m pass \033[0m")
     pass_cnt += 1
     return True
 
@@ -99,23 +98,69 @@ def input_all_test_file(in_dir, out_dir, name_append)->bool:
                     write_log(demo, log)
                     demo.kill()
                     demo.wait()
-                    print("\033[1;34m"+"test  "+name_append+"\033[0m"+"\033[1;34m proess timeout\n \033[0m")
+                    print("\033[1;34m"+"test  "+name_append+suffix+"\033[0m"+"\033[1;34m proess timeout\n \033[0m")
                     continue
-            elif demo.poll() != 0:
-                print("\033[1;34m"+"test  "+name_append+"\033[0m"+"\033[1;34m proess exit incorrectly\n \033[0m")
+                elif demo.poll() != 0:
+                    # demo.wait()
+                    print("\033[1;34m"+"test  "+name_append+suffix+"\033[0m"+"\033[1;34m proess exit incorrectly\n \033[0m")
+            elif demo.poll() == 0:
+                write_log(demo, log)
+                # demo.wait()
             out_file_name = [v for v in out_files if suffix in v]
             out_file = open(out_dir+"/"+out_file_name[0], "r", encoding="utf-8")
+            dump_file.seek(0,0)
+            check_out(dump_file, out_file, name_append+suffix)
+            dump_file.close()
+            out_file.close()
+            
+
+def group_4_test(in_dir):
+    print("\n")
+    print("test "+"group 4")
+    in_files = os.listdir(in_dir)
+    global test_cnt
+    for dir_name in in_files:
+        dir_name = in_dir+"/"+dir_name
+        name_append = dir_name
+        if os.path.isdir(dir_name):
+            dump_file_name = dir_name+"/dump"
+            log_file_name = dir_name+"/log"
+            dump_file = open(dump_file_name, "w+", encoding="utf-8")
+            dump_file.truncate(0)
+            in_file = open(dir_name+"/input", "r",encoding="utf-8")
+            log = open(log_file_name, "w+", encoding="utf-8")
+            log.truncate(0)
+            demo = Popen([test_obj, dump_file_name], stdin=in_file, stdout=PIPE, stderr=STDOUT)
+            test_cnt += 1
+            # input_test(in_file, demo)
+            time.sleep(0.1)
+            if demo.poll() == None:
+                time.sleep(1)
+                if demo.poll() == None:
+                    write_log(demo, log)
+                    demo.kill()
+                    demo.wait()
+                    print("\033[1;34m"+"test  "+name_append+"\033[0m"+"\033[1;34m proess timeout\n \033[0m")
+                    continue
+                elif demo.poll() != 0:
+                    # demo.wait()
+                    print("\033[1;34m"+"test  "+name_append+"\033[0m"+"\033[1;34m proess exit incorrectly\n \033[0m")
+            elif demo.poll() == 0:
+                write_log(demo, log)
+                # demo.wait()
+            out_file_name = dir_name+"/output"
+            out_file = open(out_file_name, "r", encoding="utf-8")
             dump_file.seek(0,0)
             check_out(dump_file, out_file, name_append)
             dump_file.close()
             out_file.close()
-            
 
 if os.path.exists(dump_dir) != True:
     os.mkdir(dump_dir)
 if os.path.exists(log_dir) != True:
     os.mkdir(log_dir)
 
+# group_4_test(group_4_test_dir)
 input_all_test_file(test_input_dir, test_output_dir, "")
 
 print("summary:\033[1;35m pass {cnt} / {total} \033[0m!".format(cnt = pass_cnt, total = test_cnt))
